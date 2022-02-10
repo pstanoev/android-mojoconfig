730a76c26a2625b3570e202317ec5b01a8c66f94
Peco Stanoev <peco.stanoev@netcetera.com>
2022-02-10 19:30:47 +0100

    [no-issue] use gradlePlugin

--- src/main/groovy/com/pstanoev/ConfigureAppTask.groovy
+++ src/main/groovy/com/pstanoev/ConfigureAppTask.groovy
@@ -3,14 +3,21 @@ package com.pstanoev
 import org.gradle.api.DefaultTask
 import org.gradle.api.Task
 import org.gradle.api.tasks.TaskAction
-
+import org.gradle.api.tasks.Input;
+import org.gradle.api.tasks.InputFile;
 
 class ConfigureAppTask extends DefaultTask {
+    @InputFile
     File configFile = new File(project.projectDir, "app.conf")
+    @Input
     boolean enabled = true
+    @Input
     boolean cacheEnabled = true
+    @Input
     String rootNodeName = "rootProject"
+    @Input
     String appNodeName = "app"
+    @Input
     boolean debug = false
 
     @TaskAction
